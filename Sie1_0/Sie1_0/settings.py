ddeffeaa6879d07bc3fe146478827ec2b10a9026
Andres Quinto <personalQuinto@gmail.com>
2018-05-30 18:25:48 -0500

    cambio bd
    de mysql a sql3

--- Sie1_0/Sie1_0/settings.py
+++ Sie1_0/Sie1_0/settings.py
@@ -79,14 +79,11 @@ WSGI_APPLICATION = 'Sie1_0.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sieData',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
