d34867c5a78142ad4832428ab64bc8c4a72983f8
Andres Quinto <personalQuinto@gmail.com>
2018-06-01 17:41:10 -0500

    img activ

--- Sie1_0/apps_s/actividad/models.py
+++ Sie1_0/apps_s/actividad/models.py
@@ -6,8 +6,8 @@ class Actividad(models.Model):
     titulo = models.CharField(max_length=120, blank=True, null=True)
     descripcion = models.TextField()
     fecha = models.DateTimeField()
-    #lugar = models.CharField(max_length=120, blank=True, null=True)
-    #imagen = models.ImageField(upload_to='images')
+    ubicacion = models.CharField(max_length=120, blank=True, null=True)
+    imagen = models.ImageField(upload_to='media/imagenes_actividad')
     iglesia = models.ForeignKey(Iglesia, on_delete=models.CASCADE)
 
     def __str__(self):
--- Sie1_0/apps_s/actividad/views.py
+++ Sie1_0/apps_s/actividad/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 
+
 # Create your views here.
 from ..actividad.models import Actividad
 
