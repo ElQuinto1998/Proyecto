a84129ec56fccfb665d914ec1b3d1b87c48ec8be
Andres Quinto <personalQuinto@gmail.com>
2018-06-03 10:21:15 -0500

    mas modulo actividad

--- Sie1_0/apps_s/actividad/models.py
+++ Sie1_0/apps_s/actividad/models.py
@@ -5,7 +5,8 @@ from ..iglesia.models import Iglesia
 class Actividad(models.Model):
     titulo = models.CharField(max_length=120, blank=True, null=True)
     descripcion = models.TextField()
-    fecha = models.DateTimeField()
+    fecha = models.DateField(blank=True, null=True)
+    hora_inicio = models.TimeField(blank=True, null=True)
     ubicacion = models.CharField(max_length=120, blank=True, null=True)
     imagen = models.ImageField(upload_to='imagenes_actividad')
     iglesia = models.ForeignKey(Iglesia, on_delete=models.CASCADE)
