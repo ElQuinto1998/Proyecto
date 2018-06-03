5f6d0b696fdc63f229d5ac831a1eb313bb0cfbfa
Andres Quinto <personalQuinto@gmail.com>
2018-06-03 12:53:51 -0500

    extend de model user

--- Sie1_0/apps_s/usuario/models.py
+++ Sie1_0/apps_s/usuario/models.py
@@ -1,16 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Usuario(models.Model):
-    nombre = models.CharField(max_length=40)
-    apellidos = models.CharField(max_length=30)
-    contrasena = models.CharField(max_length=15)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     fecha_nacimiento = models.DateField()
     telefono = models.CharField(max_length=20)
-    correo = models.EmailField()
 
     def __str__(self):
-        return self.nombre
+        return self.usuario.username
 
     class Meta:
         verbose_name = "Usuario"
