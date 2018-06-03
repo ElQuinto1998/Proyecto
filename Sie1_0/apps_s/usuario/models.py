354c932019c7b94e62dcec522adb9948862e169b
Andres Quinto <personalQuinto@gmail.com>
2018-06-03 12:58:53 -0500

    user

--- Sie1_0/apps_s/usuario/models.py
+++ Sie1_0/apps_s/usuario/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ..iglesia.models import Iglesia
 
 # Create your models here.
 class Usuario(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     fecha_nacimiento = models.DateField()
     telefono = models.CharField(max_length=20)
+    iglesia = models.ForeignKey(Iglesia, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.usuario.username
