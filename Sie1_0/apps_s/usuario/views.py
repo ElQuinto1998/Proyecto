41e7e0dffb7b5e1c66fb214bcd6a4191a915fb2f
Andres Quinto <personalQuinto@gmail.com>
2018-06-03 16:19:49 -0500

    extender modulo user django

--- Sie1_0/apps_s/usuario/views.py
+++ Sie1_0/apps_s/usuario/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 from ..actividad.models import Actividad
@@ -21,7 +23,11 @@ def registro(request):
     if request.method == 'POST':
 
         if 'telefono' in request.POST:
-
+            nombre = request.POST['nombre']
+            correo = request.POST['correo']
+            contrasenia = request.POST['contraseña']
+            user.usuario = User.objects.create_user(nombre, correo, contrasenia)
+            user.usuario.save()
             user.telefono = request.POST['telefono']
 
 
