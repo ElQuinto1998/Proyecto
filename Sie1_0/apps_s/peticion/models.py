8eed872dcb5d349cfd625df71af7db9074f89c06
Andres Quinto <personalQuinto@gmail.com>
2018-06-04 18:44:24 -0500

    ggg

--- Sie1_0/apps_s/peticion/models.py
+++ Sie1_0/apps_s/peticion/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from ..usuario.models import Usuario
 
 # Create your models here.
 class Peticion(models.Model):
     titulo = models.CharField(max_length=120, blank=True, null=True)
     descripcion = models.TextField()
     fecha = models.DateTimeField(auto_now_add=True)
-    #usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuari = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
